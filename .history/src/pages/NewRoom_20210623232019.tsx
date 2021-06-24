d70d58b55a2a5752b5bd6d48f02b1e16f675b734
Igor Silva <igorhms@hotmail.com>
2021-06-24 18:05:57 -0300

    Finalização Aula 2 - Criação de páginas com navegação, Autenticação e uso de Hooks

---
+++ .history/src/pages/NewRoom_20210623232019.tsx
@@ -0,0 +1,43 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../App';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss'
+
+export function NewRoom(){
+    const { user } = useContext(AuthContext);
+
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas sua audiência em dúvida em tempo real</p>
+            </aside>
+            <main>
+                <h1>{user?.name}</h1>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar um nova sala</h2>
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Nome da sala"                        
+                        />
+                        <Button type="submit">
+                            Criar sala
+                        </Button >
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
---
+++ .history/src/pages/Room_20210624174207.tsx
@@ -0,0 +1,3 @@
+export function Room(){
+    <h1>Hello</h1>
+}
